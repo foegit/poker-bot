88723debdef80b5a101f93c55cc994248a005ad3
Serhiy Hlavatskiy <foegit@gmail.com>
2019-02-07 00:37:38 +0200

    Check commit

--- db/database.js
+++ db/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+console.log(process.env);
+
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds054128.mlab.com:54128/pokerbot`, { useNewUrlParser: true });
 
 module.exports = mongoose.connection;
