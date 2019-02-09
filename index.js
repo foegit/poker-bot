11b9174d91096a318528006d45ef666c301a4cc0
Serhiy Hlavatskiy <foegit@gmail.com>
2019-02-10 00:34:05 +0200

    Heroku port issue. Try 1.

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ bot.command('newDeck', ({ reply }) => {
 
 bot.launch();
 
+const PORT = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello World!');
   res.end();
-}).listen(process.env.PORT);
+}).listen(PORT);
